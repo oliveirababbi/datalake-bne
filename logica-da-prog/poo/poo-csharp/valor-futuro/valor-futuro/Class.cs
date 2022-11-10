d036781aa6012ea48bccd30ebc666a38a8a4c5a0
unknown <oliveirababbi@gmail.com>
2022-11-10 11:58:53 -0300

    add: exercicio de valor futuro em poo

---
+++ logica-da-prog/poo/poo-csharp/valor-futuro/valor-futuro/Class.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valor_futuro
+{
+    internal class Investimento
+    {
+
+        public Investimento()
+        {
+            this.ValorInicial = 0;
+            this.TaxaDeJuros = 0;
+            this.PeriodoMeses = 0;
+            this.ValorFuturo = this.ValorInicial;
+        }
+
+        private Double Inicial;
+
+        public Double ValorInicial
+        {
+            get { return Inicial; }
+            set
+            {
+                if (value >= 0) Inicial = value;
+                else Inicial = 0;
+            }
+        }
+
+        private Double Taxa;
+
+        public Double TaxaDeJuros
+        {
+            get { return Taxa; }
+            set
+            {
+                if (value >= 0) Taxa = value;
+                else Taxa = 0;
+            }
+        }
+
+        private Double Periodo;
+
+        public Double PeriodoMeses
+        {
+            get { return Periodo; }
+            set
+            {
+                if (value >= 0) Periodo = value;
+                else Periodo = 0;
+            }
+        }
+
+        private Double Futuro;
+
+        public Double ValorFuturo
+        {
+            get { return Futuro; }
+            set 
+            {
+                for (int i = 0; i < Periodo; i++)
+                {
+                    Futuro *= 1 + (Taxa / 100);
+                }
+            }
+        }
+
+        //Imprimindo os dados:
+        public void Imprimir()
+        {
+            Console.WriteLine("Investimento: " + this.ValorInicial);
+            Console.WriteLine("Taxa %: " + this.TaxaDeJuros);
+            Console.WriteLine("Período (meses): " + this.PeriodoMeses);
+            Console.WriteLine("Resgate: " + this.ValorFuturo);
+        }
+
+    }
+}
+
---
+++ logica-da-prog/poo/poo-csharp/valor-futuro/valor-futuro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valor_futuro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Investimento investimento = new Investimento();
+            Console.WriteLine("CALCULANDO O RESGATE DO INVESTIMENTO...");
+
+            Console.WriteLine("Digite o valor inicial: ");
+            investimento.ValorInicial = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a taxa de juros %: ");
+            investimento.TaxaDeJuros = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de meses: ");
+            investimento.PeriodoMeses = Convert.ToDouble(Console.ReadLine());
+
+            investimento.Imprimir();
+            Console.ReadKey();
+
+
+        }
+    }
+}
